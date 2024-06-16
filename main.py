c15103f1f24d3efc15c82165ee3c96cd7215ce72
Tokhchukov Danil <79058081+makriot@users.noreply.github.com>
2024-06-16 23:42:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from src.expert_system import predict_example_fast
+
+
+app = FastAPI()
+
+
+class TextRequest(BaseModel):
+    data: str
+
+
+@app.post("/predict")
+async def predict(input: TextRequest):
+
+    result = predict_example_fast(input.data)
+
+    return {"result": result}
+
+
+def main():
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    main()
